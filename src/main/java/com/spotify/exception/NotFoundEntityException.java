c98494e0fed19000160800312e2efae596c9ac0b
Thanh562000 <Thanhnd562000@gmail.com>
2023-08-30 22:35:59 +0700

    add exception

---
+++ src/main/java/com/spotify/exception/NotFoundEntityException.java
@@ -0,0 +1,11 @@
+package com.spotify.exception;
+
+/*
+ * @project_name: spotify_backend
+ * @package: com.spotify.exception
+ * @author: Admin
+ * @version: v1.0
+ */
+
+public class NotFoundEntityException {
+}
